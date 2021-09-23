98967985dfc75b86a7705b39a874acadce0be2df
renatoGalloAlves <renato_gallo_20@hotmail.com>
2021-09-23 16:00:14 -0300

    exercicios 10 feito

---
+++ Exercicio10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace listaExercicios10 {
+    class Exercicio01 {
+        static void Main(string[] args) {
+
+            int numero, resultado;
+
+            Console.WriteLine("Digite um número:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            resultado = numero % 2;
+
+            if (resultado == 0) {
+                Console.WriteLine("O número digitado é par!");
+            } else {
+                Console.WriteLine("O número digitado é ímpar!");
+            }
+        }
+    }
+}
